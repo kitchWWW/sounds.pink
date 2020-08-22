a68e430060626e14b429aa04e0625fe371ac0033
kitchWWW <brian.e2014@gmail.com>
2020-08-21 22:46:18 -0600

    added new version

--- zdesktop/makeApp.sh
+++ zdesktop/makeApp.sh
@@ -1,1 +1,3 @@
 auto-py-to-exe
+
+pyinstaller --noconfirm --onefile --windowed --icon "/Users/brian/Documents/cs/memeWebs/sounds.pink/res/sp.icns"  "/Users/brian/Documents/cs/memeWebs/sounds.pink/zdesktop/soundsPink.py"
